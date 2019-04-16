e25842908b0108a4817fd3fc5000ad8c28c3d4f1
GiuNardo <giu.nardo1@gmail.com>
2019-04-16 10:42:04 +0200

    navbar

--- tec-abbandonate/src/app/app.module.ts
+++ tec-abbandonate/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
